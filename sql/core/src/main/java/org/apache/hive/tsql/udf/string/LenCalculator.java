e0f474d4723bd13abf5eac94769090a484bc1141
zhongdg1 <zhongdg1@lenovo.com>
2017-03-03 14:34:47 +0800

    fix len function, right blank issue

--- sql/core/src/main/java/org/apache/hive/tsql/udf/string/LenCalculator.java
+++ sql/core/src/main/java/org/apache/hive/tsql/udf/string/LenCalculator.java
@@ -1,9 +1,8 @@
 package org.apache.hive.tsql.udf.string;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hive.tsql.arg.Var;
 import org.apache.hive.tsql.udf.BaseCalculator;
-
-import java.util.List;
 
 /**
  * Created by zhongdg1 on 2017/1/19.
@@ -15,6 +14,21 @@ public class LenCalculator extends BaseCalculator {
 
     @Override
     public Var compute() {
-        return new Var(getArguments(0).getString().length(), Var.DataType.INT);
+        return new Var(trimRight(getArguments(0).getString()).length(), Var.DataType.INT);
+    }
+
+    public String trimRight(String str) {
+        if (StringUtils.isBlank(str)) {
+            return "";
+        }
+        int index = 0;
+        for (int i = str.length() - 1; i >= 0; i--) {
+            char c = str.charAt(i);
+            if (c != ' ') {
+                index = i;
+                break;
+            }
+        }
+        return str.substring(0, index + 1);
     }
 }
