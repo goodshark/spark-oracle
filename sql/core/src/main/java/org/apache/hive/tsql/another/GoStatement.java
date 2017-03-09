71fa4c749f09b18899148c8dfe154b67411fa0ca
dengrb1 <dengrb1@lenovo.com>
2017-03-09 14:09:18 +0800

    fix bug (support when parse GO block, add location sensitive NODE into it, also add GO block seq number)

--- sql/core/src/main/java/org/apache/hive/tsql/another/GoStatement.java
+++ sql/core/src/main/java/org/apache/hive/tsql/another/GoStatement.java
@@ -1,9 +1,14 @@
 package org.apache.hive.tsql.another;
 
+import org.apache.hive.tsql.cfl.GotoStatement;
+import org.apache.hive.tsql.cfl.ThrowStatement;
 import org.apache.hive.tsql.common.BaseStatement;
 import org.apache.hive.tsql.common.TreeNode;
+import org.apache.hive.tsql.ddl.CreateProcedureStatement;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -13,10 +18,18 @@ public class GoStatement extends BaseStatement {
 
     private static final String STATEMENT_NAME = "_GO_";
     private int repeat = 1;
+    // indicate goto in which GO block
+    static private int goSeq = 0;
+    // statement that location sensitive, include: CREATE PROC/THROW without args/GOTO
+    private List<CreateProcedureStatement> procList = new ArrayList<CreateProcedureStatement>();
+    private List<ThrowStatement> throwList = new ArrayList<ThrowStatement>();
+    private List<GotoStatement> gotoActionList = new ArrayList<GotoStatement>();
+    private List<GotoStatement> gotoLabelList = new ArrayList<GotoStatement>();
 
     public GoStatement() {
         super(STATEMENT_NAME);
         setNodeType(TreeNode.Type.GO);
+        goSeq += 1;
     }
 
     public void setRepeat(int n) {
@@ -63,5 +76,41 @@ public class GoStatement extends BaseStatement {
     @Override
     public BaseStatement createStatement() {
         return this;
+    }
+
+    public void addCreateProcStmt(CreateProcedureStatement stmt) {
+        procList.add(stmt);
+    }
+
+    public void addThrowStmt(ThrowStatement stmt) {
+        throwList.add(stmt);
+    }
+
+    public void addGotoAction(GotoStatement stmt) {
+        gotoActionList.add(stmt);
+    }
+
+    public void addGotoLabel(GotoStatement stmt) {
+        gotoLabelList.add(stmt);
+    }
+
+    public List<CreateProcedureStatement> getProcList() {
+        return procList;
+    }
+
+    public List<ThrowStatement> getThrowList() {
+        return throwList;
+    }
+
+    public List<GotoStatement> getGotoActionList() {
+        return gotoActionList;
+    }
+
+    public List<GotoStatement> getGotoLabelList() {
+        return gotoLabelList;
+    }
+
+    public String getGoSeq() {
+        return "GO" + Integer.toString(goSeq) + "-";
     }
 }
