18e63e1344ed932fbc46f136116137d4b1242a41
wangsm9 <wangsm9@lenovo.com>
2017-03-16 14:08:03 +0800

    整合混淆后的代码，删除core下面的hive代码

--- sql/core/src/main/java/org/apache/hive/tsql/common/ColumnDataType.java
+++
@@ -1,8 +0,0 @@
-package org.apache.hive.tsql.common;
-
-/**
- * Created by zhongdg1 on 2016/12/30.
- */
-public enum ColumnDataType {
-    STRING, LONG, DOUBLE, INT, VARCHAR, DATE, BOOLEAN, FLOAT,INTEGER,BINARY,DECIMAL,SHORT,TIMESTAMP,BYTE,NULL
-}
--- sql/core/src/main/java/org/apache/hive/tsql/func/TimeUnit.java
+++
@@ -1,8 +0,0 @@
-package org.apache.hive.tsql.func;
-
-/**
- * Created by zhongdg1 on 2017/3/15.
- */
-public enum TimeUnit {
-    YEARS, MONTHS, DAYS, HOURS, MINUTES, SECONDS
-}
