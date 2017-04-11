d2a3325f007ae38edb9570c0852333df9f8b3082
wangsm9 <wangsm9@lenovo.com>
2017-04-11 16:38:47 +0800

    add sqlserver udf

---
+++ sql/sqlserver-udf/src/main/java/org/apache/hive/extra/udf/Y.java
@@ -0,0 +1,14 @@
+package org.apache.hive.extra.udf;
+
+/**
+ * Created by wangsm9 on 2017/4/11.
+ */
+public class Y {
+    public static void main(String[] args) {
+//        DateDiffUdf udf = new DateDiffUdf();
+//        System.out.println(udf.evaluate("second", "2017-04-10 14:29:51.490", "2017-04-10 14:29:51.390"));
+//        System.out.println(udf.evaluate("second" ,"2017-12-31 14:30:01.001","2017-12-31 14:30:02.002"));
+//        System.out.println(udf.evaluate("qq" ,"2015-16-31 13:32:01.001","2018-1-1 1:1:02.002"));
+        System.out.println("foobar3");
+    }
+}
