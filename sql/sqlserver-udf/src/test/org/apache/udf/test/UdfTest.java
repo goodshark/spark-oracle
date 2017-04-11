1de30ab4950ab2f93ac7e23833cea02fbaa5c7b2
zhongdg1 <zhongdg1@lenovo.com>
2017-04-11 18:14:05 +0800

    move test to test dir

--- sql/sqlserver-udf/src/test/org/apache/udf/test/UdfTest.java
+++ sql/sqlserver-udf/src/test/org/apache/udf/test/UdfTest.java
@@ -1,4 +1,6 @@
-package org.apache.hive.extra.udf;
+package org.apache.udf.test;
+
+import org.apache.hive.extra.udf.DateDiffUdf;
 
 /**
  * Created by zhongdg1 on 2017/4/10.
@@ -8,6 +10,6 @@ public class UdfTest {
         DateDiffUdf udf = new DateDiffUdf();
 //        System.out.println(udf.evaluate("second", "2017-04-10 14:29:51.490", "2017-04-10 14:29:51.390"));
 //        System.out.println(udf.evaluate("second" ,"2017-12-31 14:30:01.001","2017-12-31 14:30:02.002"));
-        System.out.println(udf.evaluate("ms" ,"2018-1-11 1:0:1.111","2018-1-11 1:0:1.111"));
+        System.out.println(udf.evaluate("day" ,"2018-12-11 1:0:1.111","2018-1-11 1:0:1.234"));
     }
 }
