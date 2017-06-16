5dfbcca477f5493e47f617a74b5b7e7ff210d937
dengrb1 <dengrb1@lenovo.com>
2017-06-16 10:34:40 +0800

    add some TEST class(fake func)

---
+++ sql/sqlserver-analytic-engine/src/main/java/org/apache/hive/plsql/block/ExceptionHandler.java
@@ -0,0 +1,17 @@
+package org.apache.hive.plsql.block;
+
+import org.apache.hive.tsql.common.BaseStatement;
+
+/**
+ * Created by dengrb1 on 4/1 0001.
+ */
+
+public class ExceptionHandler extends BaseStatement {
+    public int execute() throws Exception {
+        return 0;
+    }
+
+    public BaseStatement createStatement() {
+        return null;
+    }
+}
