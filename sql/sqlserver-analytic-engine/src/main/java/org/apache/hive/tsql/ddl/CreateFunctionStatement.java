ea1aa13df315b3f2a85692c727eefdc2ed0867ba
Fulin FL2 Chen <chenfl2@Lenovo.com>
2017-07-20 17:51:52 +0800

    modify CreateFunction

--- sql/sqlserver-analytic-engine/src/main/java/org/apache/hive/tsql/ddl/CreateFunctionStatement.java
+++ sql/sqlserver-analytic-engine/src/main/java/org/apache/hive/tsql/ddl/CreateFunctionStatement.java
@@ -1,12 +1,15 @@
 package org.apache.hive.tsql.ddl;
 
+import javolution.io.Struct;
 import org.antlr.v4.runtime.misc.Interval;
 import org.apache.hive.basesql.func.CommonProcedureStatement;
 import org.apache.hive.plsql.PlsqlParser;
+import org.apache.hive.tsql.arg.Var;
 import org.apache.hive.tsql.common.BaseStatement;
 import org.apache.hive.tsql.common.TreeNode;
 
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by chenfl2 on 2017/7/10.
@@ -19,15 +22,39 @@ public class CreateFunctionStatement extends BaseStatement {
     private CommonProcedureStatement function;
     private SupportDataTypes returnType;
     public enum SupportDataTypes {
-        STRING, VARCHAR, LONG, DOUBLE, FLOAT, INT, INTEGER, BOOLEAN, NONE
+        STRING, CHAR,VARCHAR,VARCHAR2, LONG, DOUBLE, FLOAT, INT, INTEGER, BOOLEAN, NONE;
+
+        @Override
+        public String toString() {
+            switch (this) {
+                case INT:return "Integer";
+                case LONG:return "Long";
+                case FLOAT:return "Float";
+                case DOUBLE:return "Double";
+                case STRING:return "String";
+                case BOOLEAN:return "Boolean";
+                case INTEGER:return "Integer";
+                case CHAR:return "String";
+                case VARCHAR:return "String";
+                case VARCHAR2:return "String";
+                case NONE:return "Void";
+            }
+            return super.toString();
+        }
     }
 
     public static SupportDataTypes fromString(String str){
         if("STRING".equalsIgnoreCase(str)){
             return SupportDataTypes.STRING;
         }
+        if("CHAR".equalsIgnoreCase(str)){
+            return SupportDataTypes.CHAR;
+        }
         if("VARCHAR".equalsIgnoreCase(str)){
             return SupportDataTypes.VARCHAR;
+        }
+        if("VARCHAR2".equalsIgnoreCase(str)){
+            return SupportDataTypes.VARCHAR2;
         }
         if("LONG".equalsIgnoreCase(str)){
             return SupportDataTypes.LONG;
@@ -81,6 +108,31 @@ public class CreateFunctionStatement extends BaseStatement {
     @Override
     public int execute() throws Exception{
         return 0;
+    }
+
+    public String doCodeGen() {
+        StringBuffer sb = new StringBuffer();
+        sb.append("private ");
+        sb.append(returnType.toString());
+        sb.append(" eval(");
+        List<Var> paras = function.getInAndOutputs();
+        int i = 0;
+        for (Var var : paras) {
+            i++;
+            sb.append(fromString(var.getValueType().name()).toString());
+            sb.append(" ");
+            sb.append(var.getVarName());
+            if(i != paras.size()){
+                sb.append(",");
+            }
+        }
+        sb.append(") {\n");
+        return sb.toString();
+    }
+
+    private String genDeclare(){
+        StringBuffer sb = new StringBuffer();
+        return sb.toString();
     }
 
 }
