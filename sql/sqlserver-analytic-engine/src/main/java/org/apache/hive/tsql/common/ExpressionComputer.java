ebcbbbf0d3b9db3b94b2648dbf9eb3867149d43f
dengrb1 <dengrb1@lenovo.com>
2017-09-05 15:07:35 +0800

    fix bug a || b, when a or b is null

--- sql/sqlserver-analytic-engine/src/main/java/org/apache/hive/tsql/common/ExpressionComputer.java
+++ sql/sqlserver-analytic-engine/src/main/java/org/apache/hive/tsql/common/ExpressionComputer.java
@@ -45,10 +45,12 @@ public class ExpressionComputer {
      */
     public Var operatorConcat(Var v1, Var v2) throws Exception {
         StringBuilder sb = new StringBuilder();
-        if (v1 != null)
-            sb.append(v1.getVarValue().toString());
-        if (v2 != null)
-            sb.append(v2.getVarValue().toString());
+        if (v1 != null) {
+            sb.append(v1.getVarValue() == null ? "" : v1.getVarValue().toString());
+        }
+        if (v2 != null) {
+            sb.append(v2.getVarValue() == null ? "" : v2.getVarValue().toString());
+        }
         Var var = new Var();
         var.setDataType(Var.DataType.STRING);
         var.setVarValue(sb.toString());
