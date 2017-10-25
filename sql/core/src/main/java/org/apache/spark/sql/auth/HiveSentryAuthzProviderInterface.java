6c41479685fd2265e58370d9456246a192ca2c6f
chenfl2 <chenfl2@lenovo.com>
2017-10-25 17:30:32 +0800

    change some class info

---
+++ sql/core/src/main/java/org/apache/spark/sql/auth/HiveSentryAuthzProviderInterface.java
@@ -0,0 +1,14 @@
+package org.apache.spark.sql.auth;
+
+import org.apache.hadoop.hive.ql.metadata.AuthorizationException;
+
+import java.util.HashSet;
+
+/**
+ * Created by chenfolin on 2017/10/25.
+ */
+public interface HiveSentryAuthProviderInterface {
+
+    public void authorize(HashSet<AuthzEntity> tables, String currentdb, String username) throws AuthorizationException;
+
+}
